afda0239b9b14d5eddefb73900ea82bf5aa57c9d
baiju28feb <46016613+baiju28feb@users.noreply.github.com>
2025-03-31 09:48:04 +0530

    repo name added

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Enables static export
-  basePath: "/repository-name", // Change this to your repo name
-  assetPrefix: "/repository-name/", // Ensures assets load correctly
+  basePath: "/musicnextjs", // Change this to your repo name
+  assetPrefix: "/musicnextjs/", // Ensures assets load correctly
   images: {
     domains: ["images.unsplash.com"],
     unoptimized: true, // Required for GitHub Pages
