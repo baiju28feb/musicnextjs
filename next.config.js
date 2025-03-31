ee08127269d410f1a467ed39811ca10eb6adb433
baiju28feb <46016613+baiju28feb@users.noreply.github.com>
2025-03-31 10:04:32 +0530

    changed the assetpath

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: "export", // Enables static export
   basePath: "/musicnextjs", // Change this to your repo name
-  assetPrefix: "/musicnextjs/", // Ensures assets load correctly
+  assetPrefix: "/courses", // Ensures assets load correctly
   images: {
     domains: ["images.unsplash.com"],
     unoptimized: true, // Required for GitHub Pages
