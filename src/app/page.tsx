d353695aac8806ebbb24b574542cf5db22195781
baiju28feb <46016613+baiju28feb@users.noreply.github.com>
2025-03-31 07:29:55 +0530

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import FeaturedCourses from "@/components/FeaturedCourses"
 import AboutUs from "@/components/AboutUs"
 import {Testimonials} from "@/components/Testimonials"
 import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
     <AboutUs/>
     <Testimonials/>
     <Instructors/>
+    <Footer/>
   </main>
   );
 }
